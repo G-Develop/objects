97d432b1117dc3342b72f1e6831d68155553b013
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-08-01 22:49:27 +0000

    git for week 1 day 3

---
+++ untitled.js
@@ -0,0 +1,13 @@
+function countLetters(myString){
+	var newStr = myString.split("");
+	return newStr;
+	
+}
+var x =  countLetters("thisisatest");
+console.log(x);
+
+
+
+// var str = "How are you doing today";
+// var output = str.split("");
+// console.log(output);
